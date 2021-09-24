ce58f6ec1e955e76cd33cae84427f7bc41def727
Dylan Wiseman <dylan.wiseman22@gmail.com>
2021-09-24 15:49:09 -0600

    adding config vars

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import "./App.css";
 import Search from "./Search";
 import Results from "./Results";
 import { adzunaConfig } from "./config";
+require("dotenv").config();
 
 function App() {
   const [data, setData] = useState(null);
@@ -20,7 +21,11 @@ function App() {
     let searchTerm = encodeURI(search);
     async function getJobs() {
       const { data } = await axios.get(
-        `https://api.adzuna.com/v1/api/jobs/us/search/1?app_id=${adzunaConfig.adzunaID}&app_key=${adzunaConfig.adzunaKey}&results_per_page=50&what_phrase=${searchTerm}&what_exclude=senior%20manager%20sr%20principal%20&max_days_old=30&salary_include_unknown=1&full_time=1&permanent=1`
+        `https://api.adzuna.com/v1/api/jobs/us/search/1?app_id=${
+          process.env.ADZUNA_ID || adzunaConfig.adzunaID
+        }&app_key=${
+          process.env.ADZUNA_KEY || adzunaConfig.adzunaKey
+        }&results_per_page=50&what_phrase=${searchTerm}&what_exclude=senior%20manager%20sr%20principal%20&max_days_old=30&salary_include_unknown=1&full_time=1&permanent=1`
       );
       setData(data);
       setLoading(false);
