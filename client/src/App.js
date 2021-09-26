46940bd0df595593402eef8b229dfec311e4a87b
Dylan Wiseman <dylan.wiseman22@gmail.com>
2021-09-26 16:55:36 -0600

    my edit

--- client/src/App.js
+++ client/src/App.js
@@ -21,7 +21,7 @@ function App() {
     let searchTerm = encodeURI(search);
     async function getJobs() {
       const { data } = await axios.get(
-        `https://api.adzuna.com/v1/api/jobs/us/search/1?app_id=${process.env.ADZUNA_ID}&app_key=${process.env.ADZUNA_KEY}&results_per_page=50&what_phrase=${searchTerm}&what_exclude=senior%20manager%20sr%20principal%20&max_days_old=30&salary_include_unknown=1&full_time=1&permanent=1`
+        `https://api.adzuna.com/v1/api/jobs/us/search/1?app_id=${process.env.REACT_APP_ADZUNA_ID}&app_key=${process.env.REACT_APP_ADZUNA_KEY}&results_per_page=50&what_phrase=${searchTerm}&what_exclude=senior%20manager%20sr%20principal%20&max_days_old=30&salary_include_unknown=1&full_time=1&permanent=1`
       );
       setData(data);
       setLoading(false);
